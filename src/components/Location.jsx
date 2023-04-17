6a3adb814af981330205f724660ee800a6e8dc0f
Julio <kennkiunn@gmail.com>
2023-04-17 18:03:05 -0500

    Rick and Morty

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -10,9 +10,9 @@ const Location = ({location}) => {
 
       <h2 className='text-[27px] font-medium'>{location?.name}</h2>
       <ul className='sm:flex justify-center gap-20'>
-         <li className='font-light text-base'><span className='text-xs font-medium px-2'>Type:</span>  {location?.type}</li>
-         <li className='font-light text-base'><span className='text-xs font-medium px-2'>Dimension:</span> {location?.dimension}</li>
-         <li className='font-light text-base'><span className='text-xs font-medium px-2'>Population:</span>     {location?.residents.length}</li>
+         <li className='font-light text-[18px]'><span className='text-xs font-medium px-[6px]'>Type:</span>  {location?.type}</li>
+         <li className='font-light text-[18px]'><span className='text-xs font-medium px-[6px]'>Dimension:</span> {location?.dimension}</li>
+         <li className='font-light text-[18px]'><span className='text-xs font-medium px-[6px]'>Population:</span>     {location?.residents.length}</li>
       </ul>
 
     </section>
